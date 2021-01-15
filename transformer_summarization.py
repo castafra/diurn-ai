ecc881dc615c699d70bf5180ab8e2ff567bcaa58
François C <68732020+castafra@users.noreply.github.com>
2021-01-15 16:51:07 +0100

    Delete transformer_summarization.py

--- transformer_summarization.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-def summarize(text): 
-    summarizer = pipeline('summarization')
-
-    summary_text = summarizer(text)[0]['summary_text']
-    return summary_text
